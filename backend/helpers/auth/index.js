8e136a145ecf8174ff234c7d3d31926eecc4eb66
Shannan Galbraith <s.galbraith08@gmail.com>
2018-11-19 08:23:53 +1100

    added session setup

--- backend/helpers/auth/index.js
+++ backend/helpers/auth/index.js
@@ -1,5 +1,7 @@
 const uuidv1 = require("uuid/v1");
 const bycrypt = require("bcryptjs");
+const Cookie = require("js-cookie");
+const { getRoleFromId } = require("../roles");
 
 exports.generateId = () => {
   return uuidv1();
@@ -7,4 +9,36 @@ exports.generateId = () => {
 
 exports.generateHash = password => {
   return bycrypt.hashSync(password, 10);
+};
+
+exports.setupUserSession = async (req, username, userId, roleId, token) => {
+  req.session.loggedIn = true;
+  req.session.username = username;
+  req.session.userId = userId;
+  req.session.token = token;
+
+  const role = await getRoleFromId(roleId);
+
+  switch (role) {
+    case "Admin":
+      req.session.role = "Admin";
+      break;
+    case "Moderator":
+      req.session.role = "Moderator";
+      break;
+    case "Member":
+      req.session.role = "Member";
+      break;
+    default:
+      return null;
+  }
+
+  // set cookie for showing admin links
+  // admins routes are secure
+  if (role === "Admin" || role === "Moderator") {
+    Cookie.set("adminUI", true, { expires: 7 });
+  }
+
+  // for testing purposes
+  Cookie.set("sexy-token", token, { expires: 1 });
 };
