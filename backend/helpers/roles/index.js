9dfc4a262b0892e427b3a7e9e8b1b08b544dd422
Shannan Galbraith <s.galbraith08@gmail.com>
2018-11-19 08:24:27 +1100

    added method to return the roles title

--- backend/helpers/roles/index.js
+++ backend/helpers/roles/index.js
@@ -5,3 +5,8 @@ exports.getRoleId = async role => {
   const query = await db("roles").where("title", role);
   return query[0].id;
 };
+
+exports.getRoleFromId = async id => {
+  const query = await db("roles").where("id", id);
+  return query[0].title;
+};
