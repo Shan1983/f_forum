13de0ddc99dee2d274bcc97ebb42587589695b00
Shannan Galbraith <s.galbraith08@gmail.com>
2018-11-18 13:28:56 +1100

    added slug field

--- backend/db/migrations/20181118123148_create_category_table.js
+++ backend/db/migrations/20181118123148_create_category_table.js
@@ -2,6 +2,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable("categories", t => {
     t.uuid("id").primary();
     t.string("title").notNullable();
+    t.string("slug").notNullable();
     t.text("description");
     t.string("icon_color").defaultTo("#000");
     t.timestamps(true, true);
