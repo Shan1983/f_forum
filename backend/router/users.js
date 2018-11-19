d2e50b03e865cb090719c35487dc928eccbef30f
Shannan Galbraith <s.galbraith08@gmail.com>
2018-11-20 07:32:43 +1100

    split profile edits

--- backend/router/users.js
+++ backend/router/users.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const router = express.Router();
+const multer = require("multer");
+const moment = require("moment");
 
 const { log, isAuthenticated } = require("../middlewares");
 const controller = require("../controllers/users");
+
+// setup multer
+const storage = multer.diskStorage({
+  destination: "./uploads",
+  filename: (req, file, cb) => {
+    const filename = file.originalname;
+    const uploadedFilename = `${moment().unix()}${filename}`;
+    cb(null, uploadedFilename);
+  }
+});
+
+const upload = multer({ storage, limits: { fileSize: 1000000 } });
 
 router.get("/", isAuthenticated, log, controller.getAll);
 router.get("/admins", isAuthenticated, log, controller.getAllAdmins);
@@ -14,16 +28,32 @@ router.get("/verify/email/:token", log, controller.verifyEmail);
 router.post("/login", log, controller.login);
 router.post("/register", log, controller.register);
 router.post("/logout", log, controller.logout);
-router.post("/profile/:id/upload", isAuthenticated, log, controller.upload);
 router.post(
-  "/profile/:id/close",
+  "/profile/:id/upload",
+  isAuthenticated,
+  upload.single("avatar"),
+  log,
+  controller.upload
+);
+router.post("/:id/close", isAuthenticated, log, controller.closeAccount);
+
+router.put(
+  "/profile/:id/email",
   isAuthenticated,
   log,
-  controller.closeAccount
+  controller.updateProfileEmail
 );
-
-router.put("/profile/:id", isAuthenticated, log, controller.updateProfile);
-
-router.delete("/:id", isAuthenticated, log, controller.destroy);
+router.put(
+  "/profile/:id/password",
+  isAuthenticated,
+  log,
+  controller.updateProfilePassword
+);
+// router.put(
+//   "/profile/:id/options",
+//   isAuthenticated,
+//   log,
+//   controller.updateProfileOptions
+// );
 
 module.exports = router;
