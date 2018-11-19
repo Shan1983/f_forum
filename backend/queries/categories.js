78fd4fc7479e6eeb898286af03c98861191d3aac
Shannan Galbraith <s.galbraith08@gmail.com>
2018-11-20 08:58:34 +1100

    added queries for category

---
+++ backend/queries/categories.js
@@ -0,0 +1,43 @@
+const Joi = require("joi");
+const db = require("../db");
+
+const { insertAndValidate, softDelete } = require("./index");
+
+const schema = Joi.object().keys({
+  id: Joi.string().required(),
+  title: Joi.string().required(),
+  description: Joi.string(),
+  icon_color: Joi.string(),
+  slug: Joi.string()
+});
+
+module.exports = {
+  async findAllCategories() {
+    const categories = await db("categories").where("deleted", false);
+    return categories[0];
+  },
+
+  async findById(id) {
+    const query = await db("categories")
+      .where("id", id)
+      .where("deleted", false)
+      .first();
+
+    return query;
+  },
+
+  async update(id, category) {
+    const rows = await db("categories")
+      .where("id", id)
+      .update(category, "*");
+    return rows[0];
+  },
+
+  insert(category) {
+    return insertAndValidate("categories", category, schema);
+  },
+
+  async delete(id) {
+    return softDelete("categories", id);
+  }
+};
