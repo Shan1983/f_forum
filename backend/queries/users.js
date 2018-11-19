e79aa8ba5b8deb2a8964864fff30d3a1d5ed9f3f
Shannan Galbraith <s.galbraith08@gmail.com>
2018-11-20 07:32:15 +1100

    updated user queries

--- backend/queries/users.js
+++ backend/queries/users.js
@@ -1,7 +1,7 @@
 const Joi = require("joi");
 const db = require("../db");
 
-const { insertAndValidate } = require("./index");
+const { insertAndValidate, softDelete } = require("./index");
 const { getRoleId } = require("../helpers/roles");
 
 const schema = Joi.object().keys({
@@ -15,14 +15,17 @@ const schema = Joi.object().keys({
   role_id: Joi.string().guid({
     version: ["uuidv1"]
   }),
-  color_icon: Joi.string()
+  color_icon: Joi.string(),
+  token: Joi.string(),
+  verified: Joi.boolean()
 });
 
 module.exports = {
   async findAllAdmins() {
     const admins = await db("users")
       .where("role_id", await getRoleId("Admin"))
-      .where("role_id", await getRoleId("Moderator"));
+      .where("role_id", await getRoleId("Moderator"))
+      .where("deleted", false);
 
     if (admins.length <= 0) {
       return [{ status: 200, message: "No Admin Users." }];
@@ -32,7 +35,7 @@ module.exports = {
   },
 
   async findAllUsers() {
-    const query = await db("users");
+    const query = await db("users").where("deleted", false);
     const user = query[0];
 
     const userObj = {
@@ -46,15 +49,45 @@ module.exports = {
   },
 
   async findByEmail(email) {
+    const query = await db("users")
+      .where("email", email)
+      .where("deleted", false);
+    return query[0];
+  },
+
+  // use this only when absolutely needed
+  // does not return closed accounts.
+  async findByEmailIgnoreIfAccountClosed(email) {
     const query = await db("users").where("email", email);
     return query[0];
   },
 
-  findById(id) {
-    return db("users")
+  async getAvatar(id) {
+    const query = await db("users")
       .where("id", id)
-      .pluck("id")
+      .where("deleted", false)
+      .pluck("avatar")
       .first();
+
+    return query;
+  },
+
+  async verifyEmail(token) {
+    const query = await db("users")
+      .where("token", token)
+      .where("deleted", false)
+      .first();
+
+    return query;
+  },
+
+  async findById(id) {
+    const query = await db("users")
+      .where("id", id)
+      .where("deleted", false)
+      .first();
+
+    return query;
   },
 
   async update(id, user) {
@@ -66,5 +99,9 @@ module.exports = {
 
   insert(user) {
     return insertAndValidate("users", user, schema);
+  },
+
+  async closeAccount(id) {
+    return softDelete("users", id);
   }
 };
