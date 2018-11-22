191979a289aba1066304398dcbb493d92c7f937c
Shannan Galbraith <s.galbraith08@gmail.com>
2018-11-23 02:03:41 +1100

    updated middlewares

--- backend/router/categories.js
+++ backend/router/categories.js
@@ -1,20 +1,20 @@
 const express = require("express");
 const router = express.Router();
 
-const { log, isAuthenticated } = require("../middlewares");
+const { log, isAuthenticated, staff } = require("../middlewares");
 const controller = require("../controllers/categories");
 
 router.get("/", isAuthenticated, log, controller.getAllCategories);
 router.get(
-  "/:id/topics",
+  "/:category/topics",
   isAuthenticated,
   log,
   controller.getAllCategoryTopics
 );
 
-router.post("/", isAuthenticated, log, controller.createNewCategory);
-router.post("/:id", isAuthenticated, log, controller.removeCategory);
+router.post("/", isAuthenticated, staff, log, controller.createNewCategory);
+router.post("/:id", isAuthenticated, staff, log, controller.removeCategory);
 
-router.put("/:id", isAuthenticated, log, controller.editCategory);
+router.put("/:id", isAuthenticated, staff, log, controller.editCategory);
 
 module.exports = router;
