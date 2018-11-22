7a70431d648e7658ab3cd4f3b7e26261ea4e1263
Shannan Galbraith <s.galbraith08@gmail.com>
2018-11-23 02:09:03 +1100

    made the category title unique

--- backend/db/migrations/20181118123148_create_category_table.js
+++ backend/db/migrations/20181118123148_create_category_table.js
@@ -1,7 +1,9 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable("categories", t => {
     t.uuid("id").primary();
-    t.string("title").notNullable();
+    t.string("title")
+      .notNullable()
+      .unique();
     t.string("slug").notNullable();
     t.text("description");
     t.string("icon_color").defaultTo("#000");
