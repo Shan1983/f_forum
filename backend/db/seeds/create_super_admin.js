d3690cf3a723caf557ad97e830ff8b7d7f1c9bfa
Shannan Galbraith <s.galbraith08@gmail.com>
2018-11-23 02:06:25 +1100

    added owner/super admin user seeder

---
+++ backend/db/seeds/create_super_admin.js
@@ -0,0 +1,23 @@
+const { generateId, generateHash } = require("../../helpers/auth");
+const db = require("../index");
+
+const insertUser = [
+  {
+    id: generateId(),
+    username: "pirate pete",
+    email: "test@test.com",
+    hash: generateHash("test"),
+    role_id: "d0abc273-ed64-11e8-aa98-e36e37c6bb49",
+    verified: true
+  }
+];
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex("users")
+    .del()
+    .then(function() {
+      // Inserts seed entries
+      return knex("users").insert(insertUser);
+    });
+};
