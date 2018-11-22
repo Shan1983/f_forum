e7172ba1a19c22a2b6f5da0b909022c4080b98d9
Shannan Galbraith <s.galbraith08@gmail.com>
2018-11-23 01:59:59 +1100

    updated topics fields

--- backend/db/migrations/20181118125714_create_topics_table.js
+++ backend/db/migrations/20181118125714_create_topics_table.js
@@ -1,11 +1,19 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable("topics", t => {
     t.uuid("id").primary();
-    t.uuid("category_id").notNullable();
+    t.string("topic_color").defaultTo("#d2d2d2");
+    t.uuid("category_id")
+      .notNullable()
+      .index();
     t.string("title").notNullable();
     t.string("slug").notNullable();
-    t.uuid("user_id").notNullable();
+    t.uuid("user_id")
+      .notNullable()
+      .index();
     t.text("discussion").notNullable();
+    t.boolean("sticky").defaultTo(false);
+    t.enu("lock", ["open", "closed"]).defaultTo("open");
+    t.string("lock_reason");
     t.timestamps(true, true);
     t.boolean("deleted")
       .defaultTo(false)
