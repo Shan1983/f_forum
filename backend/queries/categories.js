c4cbf26ed62e1425150892b7d0df7f469282d587
Shannan Galbraith <s.galbraith08@gmail.com>
2018-11-24 19:16:32 +1100

    added pagination

--- backend/queries/categories.js
+++ backend/queries/categories.js
@@ -1,7 +1,7 @@
 const Joi = require("joi");
 const db = require("../db");
 
-const { insertAndValidate, softDelete } = require("./index");
+const { insertAndValidate, softDelete, paginator } = require("./index");
 
 const schema = Joi.object().keys({
   id: Joi.string().required(),
@@ -12,9 +12,67 @@ const schema = Joi.object().keys({
 });
 
 module.exports = {
-  async findAllCategories() {
-    const categories = await db("categories").where("deleted", false);
-    return categories;
+  async findAllCategories(options) {
+    // const categories = await db("categories").where("deleted", false);
+
+    const query = await db.raw(
+      `SELECT *
+      FROM categories
+      WHERE deleted = false`,
+      []
+    );
+
+    const count = await db.raw(
+      `SELECT COUNT('id')
+      FROM categories
+      WHERE deleted = false`,
+      []
+    );
+
+    const paginate = {
+      req: options.req,
+      count: count.rows[0].count,
+      query: query.rows,
+      page: options.page || 1,
+      limit: options.limit || 15
+    };
+
+    return paginator(paginate);
+  },
+
+  async findAllCategoryTopics(id) {
+    const topics = await db("topics")
+      .where("category_id", id)
+      .where("deleted", false)
+      .limit(5);
+    return topics;
+
+    // ** leave this till font end is completed **
+    // const query = await db.raw(
+    //   `SELECT *
+    //   FROM topics
+    //   WHERE category_id = ?
+    //   AND
+    //    deleted = false`,
+    //   [id]
+    // );
+
+    // const count = await db.raw(
+    //   `SELECT COUNT('id')
+    //   FROM categories
+    //   WHERE deleted = false`,
+    //   []
+    // );
+
+    // const paginate = {
+    //   req: options.req,
+    //   count: count.rows[0].count,
+    //   query: query.rows,
+    //   page: options.page || 1,
+    //   limit: options.limit || 15
+    // };
+
+    // return paginator(paginate);
   },
 
   async findById(id) {
