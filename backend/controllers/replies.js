8c2e1dd0711881b513d986e0a466f912a0fc5fb4
Shannan Galbraith <s.galbraith08@gmail.com>
2018-11-24 19:12:02 +1100

    added pagination

--- backend/controllers/replies.js
+++ backend/controllers/replies.js
@@ -7,37 +7,54 @@ const moment = require("moment");
 
 exports.getReplies = async (req, res, next) => {
   try {
-    const replies = await Reply.findByAllWithUser();
+    const options = { req, page: req.query.page, limit: req.query.limit };
+    const replies = await Reply.findByAllWithUserAndPagination(
+      req.params.topic,
+      options
+    );
 
-    console.log(replies);
+    // const r = replies.rows;
 
-    const r = replies.rows[0];
+    // if (r.length <= 0) {
+    //   res.status(404);
+    //   return next({
+    //     error: "NOTFOUND",
+    //     message: "Replies not found."
+    //   });
+    // }
 
-    if (r.length <= 0) {
+    if (replies.data === undefined) {
       res.status(404);
-      return next({
-        error: "NOTFOUND",
-        message: "Replies not found."
-      });
+      return next();
     }
 
-    const repliesObj = {
-      id: r.id,
-      reply: r.reply,
-      created: moment(r.created_at).fromNow(),
-      user: {
-        color: r.color_icon,
-        name: r.username,
-        pcount: r.post_count,
-        points: r.points,
-        avatar: r.avatar,
-        role: {
-          title: r.title
+    const repliesObj = replies.data.map(r => {
+      return {
+        id: r.id,
+        reply: r.reply,
+        created: moment(r.created_at).fromNow(),
+        user: {
+          color: r.color_icon,
+          name: r.username,
+          pcount: r.post_count,
+          points: r.points,
+          avatar: r.avatar,
+          role: {
+            title: r.title
+          }
         }
-      }
-    };
+      };
+    });
 
-    res.json(repliesObj);
+    res.json({
+      users: repliesObj,
+      meta: {
+        limit: replies.limit,
+        count: replies.count,
+        pageCount: replies.pageCount,
+        currentPage: replies.currentPage
+      }
+    });
   } catch (error) {
     res.status(400);
     next(error);
@@ -45,11 +62,21 @@ exports.getReplies = async (req, res, next) => {
 };
 exports.postreply = async (req, res, next) => {
   try {
+    const errors = Joi.validate(replySchema);
+
+    if (errors.error) {
+      res.status(400);
+      return next({
+        error: errors.error.name.toUpperCase(),
+        message: errors.error.details[0].message
+      });
+    }
+
     const replyObj = {
       id: generateId(),
       topic_id: req.params.topic,
       user_id: req.session.userId,
-      reply: re.body.reply
+      reply: req.body.reply
     };
 
     const reply = await Reply.insert(replyObj);
@@ -65,7 +92,7 @@ exports.postreply = async (req, res, next) => {
 };
 exports.editreply = async (req, res, next) => {
   try {
-    const reply = Reply.findById(req.prams.id);
+    const reply = await Reply.findById(req.params.id);
 
     if (reply === undefined) {
       res.status(404);
