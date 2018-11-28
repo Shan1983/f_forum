5de7b2cc64b183081ca3a5a0dfe1dbbed79d208c
Shannan Galbraith <s.galbraith08@gmail.com>
2018-11-28 17:07:27 +1100

    added update reward model

--- backend/queries/rewards.js
+++ backend/queries/rewards.js
@@ -1,9 +1,14 @@
 const db = require("../db");
 
 module.exports = {
+  async loadInitialRewards() {
+    return db("rewards").where("id", 1);
+  },
+
   async updateRewards(obj) {
     return db.raw(
-      `UPDATE rewards SET reply = ?, topic = ?, like = ?, create_poll = ?, answer_poll = ? WHERE id = 1;`
+      `UPDATE rewards SET reply = ?, topic = ?, like = ?, create_poll = ?, answer_poll = ? WHERE id = 1;`,
+      [obj.r, obj.t, obj.l, obj.cp, obj.ap]
     );
   }
 };
