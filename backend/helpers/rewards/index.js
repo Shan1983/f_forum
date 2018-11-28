39e000ec3e48c00c231a2b4f8d116af9c7826686
Shannan Galbraith <s.galbraith08@gmail.com>
2018-11-28 17:06:58 +1100

    added method to load rewards into memory

---
+++ backend/helpers/rewards/index.js
@@ -0,0 +1,37 @@
+const Reward = require("../../queries/rewards");
+const { logger, errorLog } = require("../logging");
+
+exports.rewardSetup = async app => {
+  try {
+    const reward = await Reward.loadInitialRewards();
+
+    app.locals.reply = reward.reply;
+    app.locals.topic = reward.topic;
+    app.locals.like = reward.like;
+    app.locals.create_poll = reward.create_poll;
+    app.locals.answer_poll = reward.answer_poll;
+
+    log = {
+      status: "OK",
+      level: "info",
+      message: "Reward data successfully loaded."
+    };
+    logger.info(log);
+
+    return true;
+  } catch (error) {
+    console.log("Reward data error");
+    console.log(error);
+    const logError = {
+      ip: "N/A",
+      user: "N/A",
+      status: 500,
+      method: "N/A",
+      url: "N/A",
+      level: "error",
+      message: "Failed to load reward data."
+    };
+    errorLog.error(logError);
+    return false;
+  }
+};
