86079f5e6764906ea9d054495c8c9d7c8cc5a0cf
Shannan Galbraith <s.galbraith08@gmail.com>
2018-11-28 17:08:14 +1100

    fixed middleware error oops

--- backend/router/subscriptions.js
+++ backend/router/subscriptions.js
@@ -2,11 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 const controller = require("../controllers/subscription");
-const { isAuthenicated, staff, log } = require("../middlewares");
+const { isAuthenticated, staff, log } = require("../middlewares");
 
-router.get("/:user", isAuthenicated, staff, log, controller.getUserSubs);
-router.get("/:topic", isAuthenicated, log, controller.isUserSub);
-router.post("/:topic", isAuthenicated, log, controller.subscribe);
-router.put("/:id", isAuthenicated, log, controller.muteSubs);
+router.get("/:user", isAuthenticated, staff, log, controller.getUserSubs);
+router.get("/:topic", isAuthenticated, log, controller.isUserSub);
+router.post("/:topic", isAuthenticated, log, controller.subscribe);
+router.put("/:id", isAuthenticated, log, controller.muteSubs);
 
 module.exports = router;
