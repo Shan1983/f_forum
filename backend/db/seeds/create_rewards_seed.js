ed3bd80c7c9dadbc4d220ffe42d9da860d9e13f7
Shannan Galbraith <s.galbraith08@gmail.com>
2018-11-28 16:23:56 +1100

    initial reward values

---
+++ backend/db/seeds/create_rewards_seed.js
@@ -0,0 +1,15 @@
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex("rewards")
+    .del()
+    .then(function() {
+      // Inserts seed entries
+      return knex("rewards").insert([
+        { id: 1, reply: 20 },
+        { id: 2, topic: 50 },
+        { id: 3, like: 10 },
+        { id: 3, create_poll: 50 },
+        { id: 3, answer_poll: 50 }
+      ]);
+    });
+};
