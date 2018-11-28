38e9d820879c8f519784ee3a90b46d9e4e7f913d
Shannan Galbraith <s.galbraith08@gmail.com>
2018-11-28 21:59:37 +1100

    added poll controller

---
+++ backend/controllers/poll.js
@@ -0,0 +1,49 @@
+exports.getPoll = async (req, res, next) => {
+  try {
+  } catch (error) {
+    res.status(400);
+    next();
+  }
+};
+exports.getAllPolls = async (req, res, next) => {
+  try {
+  } catch (error) {
+    res.status(400);
+    next();
+  }
+};
+exports.getPollResults = async (req, res, next) => {
+  try {
+  } catch (error) {
+    res.status(400);
+    next();
+  }
+};
+exports.createNewPoll = async (req, res, next) => {
+  try {
+  } catch (error) {
+    res.status(400);
+    next();
+  }
+};
+exports.voteOnPoll = async (req, res, next) => {
+  try {
+  } catch (error) {
+    res.status(400);
+    next();
+  }
+};
+exports.editPoll = async (req, res, next) => {
+  try {
+  } catch (error) {
+    res.status(400);
+    next();
+  }
+};
+exports.removePoll = async (req, res, next) => {
+  try {
+  } catch (error) {
+    res.status(400);
+    next();
+  }
+};
