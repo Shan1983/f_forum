ee9fa01cf092e8e902a93cebdceb4ff79942ad84
Shannan Galbraith <s.galbraith08@gmail.com>
2018-11-28 17:08:00 +1100

    fixed middleware error oops

--- backend/router/likes.js
+++ backend/router/likes.js
@@ -2,11 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 const controller = require("../controllers/like");
-const { isAuthenicated, log } = require("../middlewares");
+const { isAuthenticated, log } = require("../middlewares");
 
 // router.get("/meta", isAuthenicated, log, controller.getLikeMeta);
-router.post("/:topic/topic", isAuthenicated, log, controller.likeTopic);
-router.post("/:reply/reply", isAuthenicated, log, controller.likeReply);
-router.delete("/:id", isAuthenicated, log, controller.removeLike);
+router.post("/:topic/topic", isAuthenticated, log, controller.likeTopic);
+router.post("/:reply/reply", isAuthenticated, log, controller.likeReply);
+router.delete("/:id", isAuthenticated, log, controller.removeLike);
 
 module.exports = router;
