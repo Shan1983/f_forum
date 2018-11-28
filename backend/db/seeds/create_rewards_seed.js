219673417dc4cd136594bde650c0913713144795
Shannan Galbraith <s.galbraith08@gmail.com>
2018-11-28 22:54:13 +1100

    fixed - duplicate ids oops

--- backend/db/seeds/create_rewards_seed.js
+++ backend/db/seeds/create_rewards_seed.js
@@ -8,8 +8,8 @@ exports.seed = function(knex, Promise) {
         { id: 1, reply: 20 },
         { id: 2, topic: 50 },
         { id: 3, like: 10 },
-        { id: 3, create_poll: 50 },
-        { id: 3, answer_poll: 50 }
+        { id: 4, create_poll: 50 },
+        { id: 5, answer_poll: 50 }
       ]);
     });
 };
