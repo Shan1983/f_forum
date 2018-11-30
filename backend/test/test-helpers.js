f46a384ffb6e31dbeaf55d14c467bd2d3bfd30cb
Shannan Galbraith <s.galbraith08@gmail.com>
2018-11-30 23:41:28 +1100

    removed

--- backend/test/test-helpers.js
+++
@@ -1,9 +0,0 @@
-exports.setupTestDb = async db => {
-  await db.migrate.rollback();
-  await db.migrate.latest();
-  await db.seed.run();
-};
-
-exports.DbCleanup = async db => {
-  await db.migrate.rollback();
-};
