7abe22afb4ac2d5a06604cfcd09c148dc82dd244
Shannan Galbraith <s.galbraith08@gmail.com>
2018-12-01 23:07:36 +1100

    added general register user method

--- backend/test/helpers/users.js
+++ backend/test/helpers/users.js
@@ -1,7 +1,28 @@
+const faker = require("faker");
+const { generateHash } = require("../../helpers/auth");
+
 exports.login = request => {
   // log in a user
 };
 
-exports.register = request => {
+exports.register = async agent => {
   // register a user
+  const user = await agent
+    .post("/api/v1/user/register")
+    .set("content-type", "application/json")
+    .send({
+      username: faker.internet.userName(),
+      email: faker.internet.email(),
+      password: generateHash("test123")
+    });
+  return user;
+};
+
+exports.badRegistration = async (agent, data) => {
+  // register a user
+  const user = await agent
+    .post("/api/v1/user/register")
+    .set("content-type", "application/json")
+    .send(data);
+  return user;
 };
