30971cea55123bf8e242744f0a64e0ac9676a159
Shannan Galbraith <s.galbraith08@gmail.com>
2018-12-01 23:04:30 +1100

    changed seed format now all rewards live on same record

--- backend/db/seeds/create_rewards_seed.js
+++ backend/db/seeds/create_rewards_seed.js
@@ -5,11 +5,13 @@ exports.seed = function(knex, Promise) {
     .then(function() {
       // Inserts seed entries
       return knex("rewards").insert([
-        { id: 1, reply: 20 },
-        { id: 2, topic: 50 },
-        { id: 3, like: 10 },
-        { id: 4, create_poll: 50 },
-        { id: 5, answer_poll: 50 }
+        {
+          reply: 20,
+          topic: 50,
+          like: 10,
+          create_poll: 50,
+          answer_poll: 50
+        }
       ]);
     });
 };
