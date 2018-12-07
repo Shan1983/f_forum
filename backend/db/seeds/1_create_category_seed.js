2583db0c127981082d7fee27f59608b424e02b1e
Shannan Galbraith <s.galbraith08@gmail.com>
2018-12-08 01:55:04 +1100

    removed ids

--- backend/db/seeds/1_create_category_seed.js
+++ backend/db/seeds/1_create_category_seed.js
@@ -9,21 +9,18 @@ exports.seed = function(knex, Promise) {
       // Inserts seed entries
       return knex("categories").insert([
         {
-          id: 1,
           title: "test",
           slug: slug("test"),
           description: "test category",
           icon_color: randomColor()
         },
         {
-          id: 2,
           title: "Javascript",
           slug: slug("Javascript"),
           description: "Js Category",
           icon_color: randomColor()
         },
         {
-          id: 3,
           title: "spiders",
           slug: slug("spider"),
           description: "spider category",
