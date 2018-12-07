ecbc9ae071167a4f396e3e53786d5211b26f20f0
Shannan Galbraith <s.galbraith08@gmail.com>
2018-12-08 01:56:46 +1100

    completed current category tests

--- backend/test/cases/category.test.js
+++ backend/test/cases/category.test.js
@@ -81,12 +81,150 @@ describe("CATEGORY ROUTES", () => {
     });
   });
   describe("POST", () => {
-    describe("Create new categories -  Auth | staff", () => {});
+    describe("Create new categories -  Auth | staff", () => {
+      it("should create a new category", async () => {
+        const agent = getAgent(server);
+
+        const user = await login(agent, {}, "Mod");
+
+        const token = `Bearer ${user.body.token}`;
+        const createNewCategory = await agent
+          .post("/api/v1/category/")
+          .set("content-type", "application/json")
+          .set("Authorization", token)
+          .send({
+            title: "test category",
+            description: "created during testing"
+          });
+
+        createNewCategory.should.have.status(200);
+        createNewCategory.body.should.have.property("success");
+        createNewCategory.body.should.have.property("category");
+      });
+      it("should return 400 if validation fails", async () => {
+        const agent = getAgent(server);
+
+        const user = await login(agent, {}, "Mod");
+
+        const token = `Bearer ${user.body.token}`;
+        const createNewCategory = await agent
+          .post("/api/v1/category/")
+          .set("content-type", "application/json")
+          .set("Authorization", token)
+          .send({
+            title: "",
+            description: "created during testing"
+          });
+
+        createNewCategory.should.have.status(400);
+        createNewCategory.body.should.have.property("error");
+      });
+      it("prevent non staff members from creating categories", async () => {
+        const agent = getAgent(server);
+
+        const user = await login(agent, {}, "Member");
+
+        const token = `Bearer ${user.body.token}`;
+        const createNewCategory = await agent
+          .post("/api/v1/category/")
+          .set("content-type", "application/json")
+          .set("Authorization", token)
+          .send({
+            title: "test category",
+            description: "created during testing"
+          });
+
+        createNewCategory.should.have.status(401);
+        createNewCategory.body.should.have.property("error");
+      });
+    });
   });
   describe("PUT", () => {
-    describe("Edit categories -  Auth | staff", () => {});
+    describe("Edit categories -  Auth | staff", () => {
+      it("should edit a category", async () => {
+        const agent = getAgent(server);
+
+        const user = await login(agent, {}, "Mod");
+
+        const token = `Bearer ${user.body.token}`;
+        const createNewCategory = await agent
+          .put("/api/v1/category/1")
+          .set("content-type", "application/json")
+          .set("Authorization", token)
+          .send({
+            title: "test category",
+            description: "created during testing"
+          });
+
+        createNewCategory.should.have.status(200);
+        createNewCategory.body.should.have.property("success");
+        createNewCategory.body.should.have.property("category");
+      });
+      it("should return 400 if validation fails", async () => {
+        const agent = getAgent(server);
+
+        const user = await login(agent, {}, "Mod");
+
+        const token = `Bearer ${user.body.token}`;
+        const createNewCategory = await agent
+          .put("/api/v1/category/1")
+          .set("content-type", "application/json")
+          .set("Authorization", token)
+          .send({ title: "", description: "created during testing" });
+
+        createNewCategory.should.have.status(400);
+        createNewCategory.body.should.have.property("error");
+      });
+      it("prevent non staff members from editing categories", async () => {
+        const agent = getAgent(server);
+
+        const user = await login(agent, {}, "Member");
+
+        const token = `Bearer ${user.body.token}`;
+        const createNewCategory = await agent
+          .put("/api/v1/category/1")
+          .set("content-type", "application/json")
+          .set("Authorization", token)
+          .send({
+            title: "test category",
+            description: "created during testing"
+          });
+
+        createNewCategory.should.have.status(401);
+        createNewCategory.body.should.have.property("error");
+      });
+    });
   });
   describe("DELETE", () => {
-    describe("delete categories -  Auth | staff", () => {});
+    describe("delete categories -  Auth | staff", () => {
+      it("should delete a category", async () => {
+        const agent = getAgent(server);
+
+        const user = await login(agent, {}, "Mod");
+
+        const token = `Bearer ${user.body.token}`;
+        const createNewCategory = await agent
+          .delete("/api/v1/category/1")
+          .set("content-type", "application/json")
+          .set("Authorization", token);
+
+        createNewCategory.should.have.status(200);
+        createNewCategory.body.should.have.property("success");
+      });
+      it("prevent non staff members from deleting categories", async () => {
+        const agent = getAgent(server);
+
+        const user = await login(agent, {}, "Member");
+
+        const token = `Bearer ${user.body.token}`;
+        const createNewCategory = await agent
+          .delete("/api/v1/category/1")
+          .set("content-type", "application/json")
+          .set("Authorization", token);
+
+        createNewCategory.should.have.status(401);
+        createNewCategory.body.should.have.property("error");
+      });
+    });
   });
 });
