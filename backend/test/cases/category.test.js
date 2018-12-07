cfd75ff9d0b3dcc5aa75669573c5c787c4d9445e
Shannan Galbraith <s.galbraith08@gmail.com>
2018-12-08 01:26:53 +1100

    updated category tests

--- backend/test/cases/category.test.js
+++ backend/test/cases/category.test.js
@@ -4,6 +4,7 @@ const server = require("../../server");
 const db = require("../../db");
 
 const { setupTestDb, DbCleanup } = require("../helpers/dbSetup");
+const { getAgent, login } = require("../helpers/users");
 
 describe("CATEGORY ROUTES", () => {
   beforeEach(async () => {
@@ -15,8 +16,69 @@ describe("CATEGORY ROUTES", () => {
   });
 
   describe("GET", () => {
-    describe("Get All Categories - Paginated | Auth", () => {});
-    describe("Get Categories Topics - Paginated | Auth", () => {});
+    describe("Get All Categories - Paginated | Auth", () => {
+      it("should return a list all current categories", async () => {
+        const agent = getAgent(server);
+
+        const user = await login(agent, {}, "Member");
+
+        const token = `Bearer ${user.body.token}`;
+        const allCategories = await agent
+          .get("/api/v1/category?page=1&limit=15")
+          .set("content-type", "application/json")
+          .set("Authorization", token);
+
+        allCategories.should.have.status(200);
+        allCategories.body.should.have.property("categories");
+        allCategories.body.should.have.property("meta");
+      });
+      it("should return 404 if no categories found", async () => {
+        const agent = getAgent(server);
+
+        const user = await login(agent, {}, "Member");
+
+        const token = `Bearer ${user.body.token}`;
+        const allCategories = await agent
+          .get("/api/v1/category?page=1000&limit=15")
+          .set("content-type", "application/json")
+          .set("Authorization", token);
+
+        allCategories.should.have.status(404);
+      });
+    });
+    describe("Get Categories Topics - Paginated | Auth", () => {
+      it("should return a categories topics", async () => {
+        const agent = getAgent(server);
+
+        const user = await login(agent, {}, "Member");
+
+        const token = `Bearer ${user.body.token}`;
+        const categoryTopics = await agent
+          .get("/api/v1/category/1/topics?page=1&limit=15")
+          .set("content-type", "application/json")
+          .set("Authorization", token);
+
+        categoryTopics.should.have.status(200);
+        categoryTopics.body.should.have.property(
+          "category",
+          categoryTopics.body.category
+        );
+        categoryTopics.body.should.have.property("meta");
+      });
+      it("should return 404 if no topics found", async () => {
+        const agent = getAgent(server);
+
+        const user = await login(agent, {}, "Member");
+
+        const token = `Bearer ${user.body.token}`;
+        const categoryTopics = await agent
+          .get("/api/v1/category/1/topics?page=1000&limit=15")
+          .set("content-type", "application/json")
+          .set("Authorization", token);
+
+        categoryTopics.should.have.status(404);
+      });
+    });
   });
   describe("POST", () => {
     describe("Create new categories -  Auth | staff", () => {});
