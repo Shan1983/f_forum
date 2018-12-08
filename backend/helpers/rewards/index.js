a57a469e0a3b6d03529a668ad6a459eae8288ea6
Shannan Galbraith <s.galbraith08@gmail.com>
2018-12-09 02:23:51 +1100

    refactored simple reqward implementation

--- backend/helpers/rewards/index.js
+++ backend/helpers/rewards/index.js
@@ -1,37 +1,8 @@
-const Reward = require("../../queries/rewards");
-const { logger, errorLog } = require("../logging");
-
-exports.rewardSetup = app => {
-  try {
-    const reward = Reward.loadInitialRewards();
-
-    app.locals.reply = reward.reply;
-    app.locals.topic = reward.topic;
-    app.locals.like = reward.like;
-    app.locals.create_poll = reward.create_poll;
-    app.locals.answer_poll = reward.answer_poll;
-
-    log = {
-      status: "OK",
-      level: "info",
-      message: "Reward data successfully loaded."
-    };
-    logger.info(log);
-
-    return true;
-  } catch (error) {
-    console.log("Reward data error");
-    console.log(error);
-    const logError = {
-      ip: "N/A",
-      user: "N/A",
-      status: 500,
-      method: "N/A",
-      url: "N/A",
-      level: "error",
-      message: "Failed to load reward data."
-    };
-    errorLog.error(logError);
-    return false;
-  }
+module.exports = {
+  topic_reward: 50,
+  reply_reward: 20,
+  like_reward: 10,
+  create_poll_reward: 50,
+  answer_poll_reward: 50,
+  sign_up_reward: 200
 };
