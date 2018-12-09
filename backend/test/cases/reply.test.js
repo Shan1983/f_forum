8a05f9a4ccb01bd759cfd71076b65b63ced6a0cc
Shannan Galbraith <s.galbraith08@gmail.com>
2018-12-09 16:07:39 +1100

    completed current reply tests

--- backend/test/cases/reply.test.js
+++ backend/test/cases/reply.test.js
@@ -90,9 +90,78 @@ describe("REPLY ROUTES", () => {
     });
   });
   describe("PUT", () => {
-    describe("Update a reply - Auth", () => {});
+    describe("Update a reply - Auth", () => {
+      it("should update a reply", async () => {
+        const agent = getAgent(server);
+        const user = await login(agent, {}, "Member");
+
+        const token = `Bearer ${user.body.token}`;
+        const replies = await agent
+          .put("/api/v1/reply/1")
+          .set("content-type", "application/json")
+          .set("Authorization", token)
+          .send({ reply: "big long reply" });
+
+        replies.should.have.status(200);
+        replies.body.should.have.property("success");
+      });
+      it("should return 400 if validation fails", async () => {
+        const agent = getAgent(server);
+        const user = await login(agent, {}, "Member");
+
+        const token = `Bearer ${user.body.token}`;
+        const replies = await agent
+          .put("/api/v1/reply/1")
+          .set("content-type", "application/json")
+          .set("Authorization", token)
+          .send({ reply: "" });
+
+        replies.should.have.status(400);
+        replies.body.should.have.property("error");
+      });
+    });
   });
   describe("DELETE", () => {
-    describe("Delete a reply - Staff | Auth", () => {});
+    describe("Delete a reply - Staff | Auth", () => {
+      it("should remove a reply", async () => {
+        const agent = getAgent(server);
+        const user = await login(agent, {}, "Mod");
+
+        const token = `Bearer ${user.body.token}`;
+        const replies = await agent
+          .delete("/api/v1/reply/1")
+          .set("content-type", "application/json")
+          .set("Authorization", token);
+
+        replies.should.have.status(200);
+        replies.body.should.have.property("success");
+      });
+      it("should return 404 if reply not exists", async () => {
+        const agent = getAgent(server);
+        const user = await login(agent, {}, "Mod");
+
+        const token = `Bearer ${user.body.token}`;
+        const replies = await agent
+          .delete("/api/v1/reply/999")
+          .set("content-type", "application/json")
+          .set("Authorization", token);
+
+        replies.should.have.status(404);
+        replies.body.should.have.property("message");
+      });
+      it("should return 401 if unauthorized", async () => {
+        const agent = getAgent(server);
+        const user = await login(agent, {}, "Member");
+
+        const token = `Bearer ${user.body.token}`;
+        const replies = await agent
+          .delete("/api/v1/reply/1")
+          .set("content-type", "application/json")
+          .set("Authorization", token);
+
+        replies.should.have.status(401);
+        replies.body.should.have.property("error");
+      });
+    });
   });
 });
